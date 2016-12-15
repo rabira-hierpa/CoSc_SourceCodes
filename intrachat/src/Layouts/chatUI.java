a55282ebc6cb0b39a00c9bf27015bc42bdfdca4b
Rabra <rabira.hierpa@gmail.com>
2016-12-16 00:21:53 +0300

    Added the main layout

---
+++ intrachat/src/Layouts/chatUI.java
@@ -0,0 +1,18 @@
+package Layouts;
+
+import javafx.application.Application;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+public class chatUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public void chatLayout() {
+        ScrollPane mainPane = new ScrollPane();
+
+    }
+}
